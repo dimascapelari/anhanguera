42f8b8865f96780733f25c8eec2952c25f80b0fd
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-08-10 20:48:28 -0300

    canvas

---
+++ canvas/script.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("desenho");
+
+var ctx = canvas.getContext("2d");
+
+var x = 20;
+var y = 20;
+var largura = 100;
+var altura = 100;
+
+ctx.fillRect(x, y, largura, altura);
