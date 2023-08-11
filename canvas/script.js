d8413648793be177645ed3b562e183141deb7748
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-08-10 21:22:07 -0300

    canvas ctx.fillText

--- canvas/script.js
+++ canvas/script.js
@@ -7,9 +7,28 @@ var y = 20;
 var largura = 100;
 var altura = 100;
 
-ctx.fillStyle = "rgb(180,0,0)";
+ctx.fillStyle = "rgb(180, 0, 0)";
 ctx.fillRect(x, y, largura, altura);
 
 // segundo retângulo
-ctx.fillStyle = "rgba(0,0,180,0.7)";
+ctx.fillStyle = "rgba(0, 0, 180, 0.7)";
 ctx.fillRect(30, 30, 100, 100);
+
+// configurações
+var x = 100;
+var y = 100;
+var raio = 50;
+var angulo_inicio = 0;
+var angulo_fim = Math.PI * 2;
+
+// desenha um arco
+ctx.arc(x, y, raio, angulo_inicio, angulo_fim);
+
+// desenha as linhas
+ctx.stroke();
+
+// tamanho da fonte e tipo
+ctx.font = "72px serif";
+
+// inserindo o texto
+ctx.fillText("Olá mundo!", 20, 200);
