151c8738a82cf99aeabcf3d43fc49f61e60fdc57
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-08-10 21:05:42 -0300

    canvas ctx.fillStyle

--- canvas/script.js
+++ canvas/script.js
@@ -7,4 +7,9 @@ var y = 20;
 var largura = 100;
 var altura = 100;
 
+ctx.fillStyle = "rgb(180,0,0)";
 ctx.fillRect(x, y, largura, altura);
+
+// segundo retângulo
+ctx.fillStyle = "rgba(0,0,180,0.7)";
+ctx.fillRect(30, 30, 100, 100);
