8af4425bbc1910c4b7c5c22124b67f165731f7f3
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-08-10 21:27:04 -0300

    canvas ctx.fillText

--- canvas/script.js
+++ canvas/script.js
@@ -14,12 +14,14 @@ ctx.fillRect(x, y, largura, altura);
 ctx.fillStyle = "rgba(0, 0, 180, 0.7)";
 ctx.fillRect(30, 30, 100, 100);
 
-// configurações
-var x = 100;
+// configurações do arco
+var x = 200;
 var y = 100;
 var raio = 50;
 var angulo_inicio = 0;
 var angulo_fim = Math.PI * 2;
+
+// ctx.beginPath();
 
 // desenha um arco
 ctx.arc(x, y, raio, angulo_inicio, angulo_fim);
